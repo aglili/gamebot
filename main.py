9f3b09753af79907749cd603e3824f9900c541b5
aglili <cecilselorm34@gmail.com>
2023-06-06 09:38:39 +0000

    deploy error fixes

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
-from telegram import Bot,ParseMode
+from telegram import Bot, ParseMode
 from telegram.ext import CommandHandler, Updater
-from bs4 import BeautifulSoup
 import json
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
 TOKEN = os.getenv("TOKEN")
-RENDER_URL =os.getenv("RENDER_URL")
+RENDER_URL = os.getenv("RENDER_URL")
 
 bot = Bot(token=TOKEN)
-bot.set_webhook(RENDER_URL)
 
 
-def startMessage(update,context):
-    message = """
+def startMessage(update, context):
+    message = """\
     *Hello!* 👋
     I'm the Telegram Game Bot, your companion for finding free games! 🎮🆓
 
@@ -28,12 +25,10 @@ def startMessage(update,context):
 
     Feel free to explore the list and enjoy gaming! If you have any questions or need assistance, just let me know.
 
-    Happy gaming! 🎉🕹️ """
+    Happy gaming! 🎉🕹️
+    """
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id,text=message,parse_mode=ParseMode.MARKDOWN)
-    
-
-
+    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
 
 
 def fetchGames(update, context):
@@ -58,24 +53,22 @@ def fetchGames(update, context):
     context.bot.send_message(chat_id=chat_id, text="\n\n".join(messages))
 
 
-
-
-
 def main():
     # Set up the bot
-    updater = Updater(token=TOKEN,use_context=True)
+    updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Register the command handler
+    # Register the command handlers
     new_handler = CommandHandler('new', fetchGames)
+    start_handler = CommandHandler('start', startMessage)
     dispatcher.add_handler(new_handler)
-
-    welcome_handler = CommandHandler('start',startMessage)
-    dispatcher.add_handler(welcome_handler)
+    dispatcher.add_handler(start_handler)
 
     # Start the bot
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0", port=int(os.environ.get("PORT", 5000)), url_path=TOKEN)
+    updater.bot.set_webhook(RENDER_URL + TOKEN)
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
